627df750c5cea2a038c9286623cb0bbd3dc3f358
bengreenier <ben@bengreenier.com>
2019-12-30 11:51:23 +0000

    improvement(test): increase test timeouts
    This allows our integration tests to take a bit longer without timing out - which is great, given
    they depend on the network.
    
    fix #3

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,9 +1,13 @@
 import { runAction } from '../runner'
 
 describe('index', () => {
-  it('should work [integration]', async () => {
-    await runAction(
-      'https://cdn.jsdelivr.net/gh/overlayed-app/remote-overlay-test@1.1.0/index.js'
-    )
-  })
+  it(
+    'should work [integration]',
+    async () => {
+      await runAction(
+        'https://cdn.jsdelivr.net/gh/overlayed-app/remote-overlay-test@1.1.0/index.js'
+      )
+    },
+    15 * 1000 /* 15 seconds */
+  )
 })
